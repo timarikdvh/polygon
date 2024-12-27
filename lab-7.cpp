ce6a5429abdb09ca69e5df2da209a55d6f913a21
KirpoTimofey <timarikdvh@gmail.com>
2024-12-27 08:02:37 +0300

    отчет загружен, лаба сделанна

--- lab-7.cpp
+++ lab-7.cpp
@@ -3,40 +3,51 @@
 #include <algorithm>
 #include <chrono>
 #include <random>
+#include <math.h>
+
 using namespace std;
 std::random_device r;
 std::default_random_engine e(r());
-std::uniform_int_distribution<int> dist(1,100);
+std::uniform_int_distribution<int> dist(0,20);
 
-int maxCoins(vector<int>& nums) {
-    nums.insert(nums.begin(), 1);
-    nums.push_back(1);
-    int n = nums.size();
+int minTaps(int n, vector<int>& ranges) {
+    vector<int> maxRange(n + 1, 0);
 
-    vector<vector<int>> dp(n, vector<int>(n, 0));
+    for (int i = 0; i <= n; ++i) {
+        int left = max(0, i - ranges[i]);
+        int right = min(n, i + ranges[i]);
+        maxRange[left] = max(maxRange[left], right);
+    }
 
-    for (int length = 2; length < n; ++length) {
-        for (int left = 0; left < n - length; ++left) {
-            int right = left + length;
-            for (int k = left + 1; k < right; ++k) {
-                dp[left][right] = max(
-                        dp[left][right],
-                        dp[left][k] + nums[left] * nums[k] * nums[right] + dp[k][right]);
+    int taps = 0, currentEnd = 0, farthest = 0;
+
+    for (int i = 0; i <= n; ++i) {
+        if (i > farthest) {
+            return -1;
+        }
+
+        farthest = max(farthest, maxRange[i]);
+
+        if (i == currentEnd) {
+            ++taps;
+            currentEnd = farthest;
+            if (currentEnd >= n) {
+                break;
             }
         }
     }
 
-    return dp[0][n - 1];
+    return taps;
 }
 
 int main(){
     vector<int> nums;
-    for (int i = 100; i < 10000; i+=100) {
+    for (int i = pow(10, 7); i < pow(10, 9); i+=pow(10, 7)) {
         for (int j = 0; j < i; j++){
             nums.push_back(dist(e));
         }
         auto start_time = chrono::high_resolution_clock::now();
-        maxCoins(nums);
+        cout << minTaps(i - 1, nums) << endl;
         auto end_time = chrono::high_resolution_clock::now();
         chrono::duration<double> elapsed_time = end_time - start_time;
         cout << "Time: " << elapsed_time.count() << " seconds. Elems: " << i << endl;
